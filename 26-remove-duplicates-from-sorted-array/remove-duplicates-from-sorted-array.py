e87c5e01b736ec77e1d0a146222c30a0833db605
TProgrammer <77713508+TProgramer@users.noreply.github.com>
2024-07-09 22:45:10 +0900

    Time: 68 ms (90.33%) | Memory: 18.1 MB (9.74%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -3,7 +3,11 @@ class Solution:
         
         # index to track new array
         idx = 1
+
+        # update new array in loop
         for i in range(1, len(nums)):
+
+            # only update when unique
             if nums[i - 1] != nums[i]:
                 nums[idx] = nums[i]
                 idx += 1
